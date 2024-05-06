a37667462429620e277656127a41487d21f959de
Farrouault-Julien <julien.farrouault@gmail.com>
2024-05-06 16:03:22 +0200

    ok

---
+++ jfwd/src/App.tsx
@@ -0,0 +1,15 @@
+import Header from '../src/components/Header.tsx';
+import './App.css'
+
+function App() {
+  
+
+  return (
+        <div className="layout">
+          <Header />
+        </div>
+      
+  )
+}
+
+export default App
---
+++ jfwd/src/components/Header.tsx
@@ -0,0 +1,9 @@
+import { Component } from 'react'
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className='p-4 bg-blue-500'>Header</div>
+    )
+  }
+}
