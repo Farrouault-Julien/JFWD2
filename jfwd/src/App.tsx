3203f24166ca7370e04fd14d0449774c02a38f3c
Farrouault-Julien <julien.farrouault@gmail.com>
2024-05-06 19:05:36 +0200

    install ok

--- jfwd/src/App.tsx
+++ jfwd/src/App.tsx
@@ -1,14 +1,18 @@
-import Header from '../src/components/Header.tsx';
+import Header from './components/Header'
 import './App.css'
 
 function App() {
   
 
   return (
-        <div className="layout">
-          <Header />
-        </div>
-      
+    <div className="App">
+      <div className="home_wrapper h-lvh flex flex-col justify-between ">
+        <section className='px-10 '>
+        <Header />
+      </section>
+
+      </div>
+    </div>
   )
 }
 
--- jfwd/src/components/Header.tsx
+++ jfwd/src/components/Header.tsx
@@ -1,9 +1,36 @@
-import { Component } from 'react'
+import React, { useState } from 'react';
 
-export default class Header extends Component {
-  render() {
-    return (
-      <div className='p-4 bg-blue-500'>Header</div>
-    )
-  }
-}
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="header_wrapper">
+      <header className="header flex justify-between pt-2 ">
+        <h1 className="header_title">JFWD</h1
+        ><h1>@2024</h1>
+        <button onClick={toggleMenu} className="burger">
+          ☰
+        </button>
+        {isOpen && (
+          <nav className="header_nav">
+            <button onClick={toggleMenu} className="close">
+              ✖
+            </button>
+            <ul className='flex justify-center '>
+              <li className='padding-menu'><a href="#">Home</a></li>
+              <li className='padding-menu'><a href="#">works</a></li>
+              <li><a href="#">Contact</a></li>
+            </ul>
+          </nav>
+        )}
+        
+      </header>
+    </div>
+  );
+};
+
+export default Header;
